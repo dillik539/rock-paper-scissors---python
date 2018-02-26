179cfbf498e9c0d8e56f2c95c84f8a9c19259b73
clara <clara@clara.com>
2018-02-26 10:29:17 -0600

    restructure and tests

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -9,12 +9,18 @@ def main():
         compGuess = getChoice(randomNumber)
 
         userGuess = getUserInput()
-        print(getResult(compGuess,userGuess))
+
+        result = getResult(compGuess,userGuess)
+
+        output = formatWinner(result, compGuess, userGuess)
+        print(output)
 
         playAgain = input("Do you want to play again? Enter y for yes: ")
         playAgain = playAgain.lower()
         if(playAgain!="y"):
             print("Thank you for playing. Come again.")
+
+
 def getChoice(number):
     if number == 1:
         choice = "Rock"
@@ -24,14 +30,32 @@ def getChoice(number):
         choice = "Scissors"
     return choice
 
+
 def getResult(computerGuess, userGuess):
+
+    # Figure out who won
+
     if computerGuess == userGuess:
-        output = "It's a tie. Play again.\nYou chose " + userGuess + ".\nThe computer also chose " + computerGuess + "."
-        return output
+        return 'Tie'
 
     elif ((computerGuess == "Rock" and userGuess == "Scissors") or
     (computerGuess == "Paper" and userGuess == "Rock") or
     (computerGuess == "Scissors" and userGuess == "Paper")):
+        return 'Computer'
+
+    else:
+        return 'User'
+
+
+def formatWinner(winner, computerGuess, userGuess):
+
+    # Use game results to display user-friendly message
+
+    if winner == 'Tie':
+        output = "It's a tie. Play again.\nYou chose " + userGuess + ".\nThe computer also chose " + computerGuess + "."
+        return output
+
+    if winner == 'Computer':
         output = "Oops! The computer wins.\nYou chose " + userGuess + ".\nThe computer chose " + computerGuess + "."
         return output
 
@@ -39,7 +63,14 @@ def getResult(computerGuess, userGuess):
         output = "Congratulations! You win.\nYou chose " + userGuess + ".\nThe computer chose " + computerGuess + "."
         return output
 
+
 def getUserInput():
+
+    # Suggestion: tell user what they need to enter
+    # User should enter 1 = rock, 2 = paper or 3 = scissors
+    # Add some validation
+    # Then you can use getChoice to turn the number into "rock" or "paper" or "scissors"
+
     user = input("Enter your choice: ")
     user = user.strip()
 
@@ -47,5 +78,8 @@ def getUserInput():
     rem_letters = user[1:].lower()
     user = first_letter + rem_letters
     return user
+
+
+
 if __name__ == '__main__':
     main()
